b612154ad2c806864848b12d803dc2b44edf8b71
Sam Winslow <sammywinslow@gmail.com>
2019-11-12 15:30:55 -0500

    Temp commit without about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -9,27 +9,27 @@ import Modal from 'containers/modal';
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
 
-const About = ({ data }) => (
+const About = (/*{ data }*/) => (
   <Layout>
     <Box>
       <Title as="h2">
       Me
       </Title>
       <p>Go placidly amid the noise and haste, and remember what peace there may be in silence. As far as possible without surrender be on good terms with all persons. Speak your truth quietly and clearly; and listen to others, even the dull and the ignorant; they too have their story.</p>
-      <Gallery data={data} />
-      <Img fixed={data.file.childImageSharp.fixed} />
+      {/*<Gallery data={data} />*/}
+  {/* <Img fixed={data.file.childImageSharp.fixed} /> */}
     </Box>
   </Layout>
 );
 
-About.propTypes = {
+/*About.propTypes = {
   data: PropTypes.object.isRequired,
-};
+};*/
 
 export default About;
 
 // where is this file??
-export const query = graphql`
+/*export const query = graphql`
   query {
     file(relativePath: { eq: "/prof-vested.jpg" }) {
       childImageSharp {
@@ -39,4 +39,4 @@ export const query = graphql`
       }
     }
   }
-`
+`*/
