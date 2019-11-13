387a4be26e5d1415b440ec4ccabcbbdeacde164c
Sam Winslow <sammywinslow@gmail.com>
2019-11-13 03:07:03 -0500

    mobile header testing

--- src/components/header/header.css.js
+++ src/components/header/header.css.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
+import MEDIA from '../../helpers/mediaTemplates';
 
 export const Container = styled.header`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
   padding: 1rem 2rem;
@@ -9,4 +11,7 @@ export const Container = styled.header`
   a {
     text-decoration: none;
   }
+  ${MEDIA.PHONE`
+    justify-content: center;
+  `};
 `;
